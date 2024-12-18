d0285d96d6917f84281770c30cc6dac3669a1354
Dan Caseley <dan@caseley.me.uk>
2024-12-18 12:21:56 +0000

    Christmas Logging

---
+++ christmas/README.md
@@ -0,0 +1,11 @@
+# Getting logs from loops
+
+This is a simple example of how to get logs from loops in a smart way.
+
+It's also festive themed, because it's Christmas!
+
+## Usage
+
+```bash
+maestro test 12days.yaml
+```
---
+++ christmas/setup.js
@@ -0,0 +1,35 @@
+output.intro = function (day) {
+  var daySuffix = "";
+  switch (day) {
+    case 1:
+      daySuffix = "st";
+      break;
+    case 2:
+      daySuffix = "nd";
+      break;
+    default: // I know, 21 won't work, but I only need to care about 1-12
+      daySuffix = "th";
+  }
+  return (
+    "On the " +
+    day +
+    daySuffix +
+    " day of Christmas, my true love gave to me..."
+  );
+};
+
+output.gifts = [
+  "",
+  "A partridge in a pear tree",
+  "Two turtle doves",
+  "Three French hens",
+  "Four calling birds",
+  "Five golden rings",
+  "Six geese a-laying",
+  "Seven swans a-swimming",
+  "Eight maids a-milking",
+  "Nine ladies dancing",
+  "Ten lords a-leaping",
+  "Eleven pipers piping",
+  "Twelve drummers drumming",
+];
